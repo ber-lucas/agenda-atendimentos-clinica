85ee03bb13794b16868ac2c2d1cdfab314ac6a72
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2024-02-28 16:23:16 -0300

    :bug: fix: Erro no cadastro da rota Cadastrando a rota no arquivo server.ts para que o Fastify a reconheça

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
+import { createLocal } from './routes/local/create-local';
 
 const app = fastify();
+
+app.register(createLocal);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
 });
-
